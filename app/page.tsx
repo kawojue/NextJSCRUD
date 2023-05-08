f3ef52a1605e7017e9064d8e70c7af8e952fa7e0
Raheem Kawojue <kawojue08@gmail.com>
2023-05-08 01:30:46 +0100

    [add] metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link"
-import type, { Metadata } from 'next'
+import { Metadata } from 'next'
 
-const metadata: Metadata = {
+export const metadata: Metadata = {
     title: 'Homepage',
     description: 'Welcome!'
 }
