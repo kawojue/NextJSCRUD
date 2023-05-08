479cdbefc63dfd3802b7af7e1a017f3883cd4473
Raheem Kawojue <kawojue08@gmail.com>
2023-05-08 23:58:27 +0100

    [add] header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import  Header from '@/components/Header'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,9 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <nav>
-          <h1>A Navbar</h1>
-        </nav>
+        <Header />
         {children}
       </body>
     </html>
