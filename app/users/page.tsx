cd6597b8e529920fe54d3efae56b1cceebd02e5c
Raheem Kawojue <kawojue08@gmail.com>
2023-05-11 13:32:33 +0100

    [add] styles

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -14,10 +14,13 @@ const page = async () => {
             <Link className="mb-4 font-medium" href="/">
                 Go back Home
             </Link>
-            <article className="flex flex-col gap-2 w-fit">
-                {users.map((user: any) => (
-                    <Link key={user.id} href={`/users/${user.id}`}>{user.name}</Link>
-                ))}
+            <article className="w-[95vw] max-w-[500px] mx-auto bg-clr-0 px-3 py-5 rounded-lg">
+                <div className="grid grid-cols-2 gap-2 place-items-center">
+                    {users.map((user: any) => (
+                        <Link key={user.id} href={`/users/${user.id}`}
+                        className="px-2 py-1 rounded-md bg-white">{user.name}</Link>
+                    ))}
+                </div>
             </article>
         </section>
     )
