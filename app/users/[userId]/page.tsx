977984044d410b9c6a81cac5f7bc63858152281c
Raheem Kawojue <kawojue08@gmail.com>
2023-05-12 02:04:44 +0100

    ...

--- app/users/[userId]/page.tsx
+++ app/users/[userId]/page.tsx
@@ -1,4 +1,6 @@
 import getUser from "@/lib/getUser"
+import getUserPosts from "@/lib/getUserPosts"
+import UserPosts from '@/app/users/[userId]/posts/page'
 
 interface User {
     params: {
@@ -8,11 +10,17 @@ interface User {
 
 const page = async ({ params: { userId } }: User) => {
     const user = await getUser(userId)
+    const userPosts = await getUserPosts(userId)
 
     return (
-        <>
+        <section>
             {JSON.stringify(user)}
-        </>
+            <h3>{await user.name}</h3>
+            <p>@{user.username}</p>
+            <h4>{user.email}</h4>
+            <p>{user.address.street}</p>
+            <UserPosts posts={userPosts} />
+        </section>
     )
 }
 
